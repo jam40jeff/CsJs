a487c12b43779c5d3a75ced2ba55e599d3f6f8c7
Jeffrey Morse <jam40jeff@gmail.com>
2013-07-29 15:31:29 -0400

    Cleanup code.

--- src/Examples/CalculatorsAndStopwatch/UI/QueryableGridPage.cs
+++ src/Examples/CalculatorsAndStopwatch/UI/QueryableGridPage.cs
@@ -37,13 +37,12 @@ namespace MorseCode.CsJs.Examples.CalculatorsAndStopwatch.UI
 
 			_grid.BindDataWithSorting(dataContext, new SampleItemCollectionItem(null, -1, null, null, false), d => d.QueryableItems, d => new ReadOnlyProperty<List<IGridColumn<SampleItemCollectionItem>>>(new List<IGridColumn<SampleItemCollectionItem>>
 				{
-					//TODO: SaltarelleCompiler issue #227
 					new GridBoundTextColumn<SampleItem, int>(o => o.Id, v => "ID #" + v) { HeaderText = "ID" },
-					new GridBoundTextColumn<SampleItemCollectionItem, string>(o => o.Name) { HeaderText = "Name" },
-					new GridBoundTextColumn<SampleItemCollectionItem, string>(o => o.Something) { HeaderText = "Something Else" },
-					new GridBoundBooleanColumn<SampleItemCollectionItem>(o => o.Boolean) { HeaderText = "A Boolean Value!", DisplayMode = GridBooleanBoundColumnDisplayMode.Text },
+					new GridBoundTextColumn<SampleItem, string>(o => o.Name) { HeaderText = "Name" },
+					new GridBoundTextColumn<SampleItem, string>(o => o.Something) { HeaderText = "Something Else" },
+					new GridBoundBooleanColumn<SampleItem>(o => o.Boolean) { HeaderText = "A Boolean Value!", DisplayMode = GridBooleanBoundColumnDisplayMode.Text },
 					new GridBoundBooleanColumn<SampleItem>("Boolean2", o => o.Boolean) { HeaderText = "A Boolean Value With Custom Text", DisplayMode = GridBooleanBoundColumnDisplayMode.Text, TrueText = "Absolutely", FalseText = "No Way" },
-					new GridBoundBooleanColumn<SampleItemCollectionItem>("Boolean3", o => o.Boolean) { HeaderText = "Boolean With Checkbox", DisplayMode = GridBooleanBoundColumnDisplayMode.CheckBox },
+					new GridBoundBooleanColumn<SampleItem>("Boolean3", o => o.Boolean) { HeaderText = "Boolean With Checkbox", DisplayMode = GridBooleanBoundColumnDisplayMode.CheckBox },
 					new SampleGridColumn("Sample") { HeaderText = "Custom Column" },
 					new GridButtonColumn<SampleItemCollectionItem>("Button", (item, button) => button.BindText(item, d2 => d2.Id, v => "Delete Row With ID " + v), item => item.Delete) { HeaderText = "Delete" }
 				}));
